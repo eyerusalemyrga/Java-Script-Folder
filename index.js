80e972d0afdd76c0965f5f8eab63908b824705a3
Eyerusalem Yrga <eyerusalemykahsay>
2025-04-01 11:15:51 +0300

    added JavaScript files

---
+++ index.js
@@ -0,0 +1,20 @@
+let firstString = `The child walked to the market`;
+console.log(firstString.charAt(0).toUpperCase() + firstString.slice(1));
+
+/*let charToAdd = 'x';*/
+/*let index = 3;*/
+let firstName="Eyerusalem";
+console.log(firstName.slice(0, 3) + 'x'+ firstName.slice(3));
+let container = "hello world, hello JavaScript!";
+let theWord = `hello`;
+console.log(container.toLowerCase().split(theWord.toLowerCase()).length - 1);
+console.log(firstName.slice(7));
+console.log(firstName.slice(0, 3));
+console.log([...firstName].sort().join(''));
+let lastName="Yrga Kahsay";
+console.log(lastName.replace(/\s+/g, ''));
+let original= `greeting`;
+let charWanted = 't';
+console.log(original.indexOf(charWanted));
+console.log(firstName.slice(2));
+console.log(firstName.slice(0, 8));
